e4e792830225758588f6b66bfdec53418eee95d5
natashanikolic <natashanikolic@users.noreply.github.com>
2016-03-19 18:28:08 +0100

    Committing initial project source files

---
+++ FileConversionCloudApp/FileConversionCommon/FileContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConversionCommon
+{
+    public class FileContext : DbContext
+    {
+        //this constructor is used by the web project getting connection string from web config
+        public FileContext()
+            : base("name=FileConversionContext")
+        {
+        }
+
+        //this constructor is used by the worker role
+        public FileContext(string connString)
+            : base(connString)
+        {
+        }
+
+        public System.Data.Entity.DbSet<File> Files { get; set; }
+    }
+}
---
+++ FileConversionCloudApp/FileConversionWebRole/Global.asax.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace FileConversionWebRole
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            InitializeStorage();
+        }
+
+        private void InitializeStorage()
+        {
+            // Open storage account using credentials from .cscfg file.
+            var storageAccount = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("StorageConnectionString"));
+
+            Trace.TraceInformation("Creating files blob container");
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var filesBlobContainer = blobClient.GetContainerReference("files");
+            if (filesBlobContainer.CreateIfNotExists())
+            {
+                // Enable public access on the newly created "files" container.
+                filesBlobContainer.SetPermissions(
+                    new BlobContainerPermissions
+                    {
+                        PublicAccess = BlobContainerPublicAccessType.Blob
+                    });
+            }
+
+            Trace.TraceInformation("Creating files queue");
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            var filesQueue = queueClient.GetQueueReference("files");
+            filesQueue.CreateIfNotExists();
+
+            Trace.TraceInformation("Storage initialized");
+        }
+    }
+}
