453e8c7cdb39860e958332c888b3718e58f8673e
Clara Galea <c_v_galea@hotmail.com>
2016-03-31 15:23:19 +0200

    progress bar ui part + SSE

---
+++ FileConversionCloudApp/FileConversionWebRole/ProgressHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FileConversionWebRole
+{
+    public class ProgressHub : Hub
+    {
+        public async Task<string> DoLongRunningThing(IProgress<int> progress)
+        {
+            for (int i = 0; i <= 100; i += 5)
+            {
+                await Task.Delay(200);
+
+                progress.Report(i);
+            }
+            return "Job complete!";
+        }
+    }
+}
